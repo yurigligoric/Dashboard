5d21cd4b5947f3f6ab33dbd88eeca0d38c5fc1c8
yurigligoric <yurigligoric@gmail.com>
2023-02-16 03:38:45 +0800

    sidebar setup not finished

--- src/scenes/global/SideBar.jsx
+++ src/scenes/global/SideBar.jsx
@@ -1,4 +1,23 @@
 import React from 'react'
+import { ProSideBar, Menu, MenuItem} from 'react-pro-sidebar'
+import 'react-pro-sidebar/dist/css/styles.css';
+import {Box, IconButton, Typography, useTheme} from '@mui/material'
+import { Link } from 'react-router-dom'
+import { tokens } from '../../theme';
+import  LightModeOutlinedIcon  from "@mui/icons-material/LightModeOutlined"
+import  LightModeOutlinedIcon  from "@mui/icons-material/LightModeOutlined"
+import  LightModeOutlinedIcon  from "@mui/icons-material/LightModeOutlined"
+import  LightModeOutlinedIcon  from "@mui/icons-material/LightModeOutlined"
+import  LightModeOutlinedIcon  from "@mui/icons-material/LightModeOutlined"
+import  LightModeOutlinedIcon  from "@mui/icons-material/LightModeOutlined"
+import  LightModeOutlinedIcon  from "@mui/icons-material/LightModeOutlined"
+import  LightModeOutlinedIcon  from "@mui/icons-material/LightModeOutlined"
+import  LightModeOutlinedIcon  from "@mui/icons-material/LightModeOutlined"
+import  LightModeOutlinedIcon  from "@mui/icons-material/LightModeOutlined"
+import  LightModeOutlinedIcon  from "@mui/icons-material/LightModeOutlined"
+import  LightModeOutlinedIcon  from "@mui/icons-material/LightModeOutlined"
+import  LightModeOutlinedIcon  from "@mui/icons-material/LightModeOutlined"
+import  LightModeOutlinedIcon  from "@mui/icons-material/LightModeOutlined"
 
 const SideBar = () => {
   return (
